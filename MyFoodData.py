685bb6b0718dce2004793f45dce6e5ca6f9472c4
Rasmane-GitHub <132719909+Rasmane-GitHub@users.noreply.github.com>
2023-12-01 19:14:48 -0800

    12/01/23_Notes added
    12/01/23_Notes added

--- MyFoodData.py
+++ MyFoodData.py
@@ -1,3 +1,12 @@
+# USE NOTES FOR PRESENTATION # 12/01/23
+
+# Import the necessary libraries
+# Pandas --- It is used for manipulation
+# openpyxl --- It is used for handling Excel files
+# sqlalchemy --- SQL toolkit and Object-relational Mapping library that provides a set of
+# application programming Interface  for connecting relational databases.
+# matplotlib.pyplot --- It is used for creating visualizations.
+
 import pandas as pd
 import openpyxl
 import xlrd
@@ -8,7 +17,6 @@ from sqlalchemy import text
 import pymysql
 
 from matplotlib import pyplot as plt
-
 # Or import matplotlib.pyplot as plt
 
 hostname = "127.0.0.1"
@@ -16,63 +24,76 @@ uname = "root"
 pwd = ""
 dbname = "fooddatamine"
 
+# 12/01/23
+# Create a connection to the MySQL database
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
                        .format(host=hostname, db=dbname, user=uname, pw=pwd))
-# Import MyFoodData website address
+
+# 12/01/23
+# Import MyFoodData from Download folder on my local machine
 tables = pd.read_excel(r"C:\Users\Master_Ras\Downloads\MyFoodData.xlsx",
                        sheet_name='Food',
                        header=3)
 print(tables)
 
+# 12/01/23
+# Connect to the database and store the data from tables into temporary table (temp_food)
 connection = engine.connect()
 tables.to_sql('temp_food', con=engine, if_exists='append')
 
+# 12/01/23
+# Create a new table (my_food) with the same structure as temp_food, then insert the data from temp_food into my_food.
+# Drop the temporary table (temp_food)
+# Close the database connection
 connection.execute(text('CREATE TABLE my_food like temp_food'))
 connection.execute(text('INSERT INTO my_food(ID, name, `Food Group`, Calories, `Fat (g)`, `Protein (g)`, `Carbohydrate (g)`, `Sugars (g)`) SELECT DISTINCT ID, name, `Food Group`, Calories, `Fat (g)`, `Protein (g)`, `Carbohydrate (g)`, `Sugars (g)` FROM temp_food'))
 connection.execute(text('DROP TABLE temp_food'))
 connection.close()  # Close extension to database
 
-# New code 11/30/23
+
+######################################################################################################################
+
+# NEW CODE 11/30/23
 data = ['my_food']
-df = pd.read_sql_table('my_food', engine.connect())
+
+# 12/01/23
+# Create dataFrame with column names
+df = pd.DataFrame(data, columns=['ID', 'name', 'Food Group', 'Calories', 'Fat', 'Protein', 'Carbohydrate', 'Sugars'])
 print(df)
+
+# df = pd.read_sql_table('my_food', engine.connect()) -
+# print(df) -
 
 df.plot(x='Waffle Plain Frozen Ready-To-Heat Microwave', y='Calories')
 plt.xlabel('Waffle Plain Frozen Ready-To-Heat Microwave')
 plot.ylabel('Calories')
 plt.title('Waffle Plain Frozen Ready-To-Heat Microwave', 'Calories')
 plot.show()
+
 # df.plot(x='Waffle Plain Frozen Ready-To-Heat Microwave', y='Calories')
 # plt.xlable('Waffle Plain Frozen Ready-To-Heat Microwave')
 # plot.ylabel('Calories')
 # plt.tile('Waffle Plain Frozen Ready-To-Heat Microwave', 'Calories')
 # plot.show()
-# print(df)
-#
-# plt.figure(figsize=(24, 24))
-# plt.plot(df['Date'], df['Diesel_Price'])
-# plt.xlabel('Date')
-# plt.xticks(rotation=90)
-# plt.ylabel('Open')
-# plt.title('Diesel Over Time', fontsize=16)
-# plt.show()
-#
 
-
-# WRITE YOUR NEW CODE HERE
-# DATA
-
-# plt.plot(temp_food.ID, MyFoodData.name)
-# plot.show()
-#
-# plt.figure(figsize=(6, 6))
-# plt.bar(df['Calories'], df['sugar'])
+# NEW
+# plt.figure(figsize=(10, 6))
 # plt.bar(df['name'], df['Calories'])
-# plt.xlabel('Food Name')
-# plt.xticks(rotation=90)
+# plt.xlabel('Food Items')
 # plt.ylabel('Calories')
-# plt.title('Calories in Foods')
+# plt.title('Calories in Food Items')
+# plt.xticks(rotation=90)
+# plt.tight_layout()
 # plt.show()
 
-# connection.close()
+plt.figure(figsize=(6, 6))
+plt.bar(df['Calories'], df['sugar'])
+plt.bar(df['name'], df['Calories'])
+plt.xlabel('Food Name')
+plt.xticks(rotation=90)
+plt.ylabel('Calories')
+plt.title('Calories in Foods')
+plt.show()
+
+connection.close()
 #Rasy and Rosa
